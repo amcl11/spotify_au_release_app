d58153568ef778cae82045a9809ddb3a1aac0659
amcl11 <132857824+amcl11@users.noreply.github.com>
2024-02-14 17:26:22 +1100

    updated playlist function to include artist and title

--- playlist_membership_check.py
+++ playlist_membership_check.py
@@ -1,31 +1,32 @@
-# This function is designed to find the position of a specific track within a Spotify playlist. 
-# It requires the Spotify API credentials (`client_id`, `client_secret`, and `redirect_uri`), a `playlist_id`, and a `track_id` to operate.
-
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-
-def get_tracks_positions_in_playlists(client_id, client_secret, redirect_uri, playlists_dict, track_ids):
-    scope = 'playlist-read-private'
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
-                                                   client_secret=client_secret,
-                                                   redirect_uri=redirect_uri,
-                                                   scope=scope))
-    track_positions = {track_id: {} for track_id in track_ids}  # Dictionary to store positions
+def get_tracks_positions_in_playlists(sp, playlists_dict, track_ids):
+    track_positions = {}
+    
+    for track_id in track_ids:
+        # Initialize an empty dict for each track_id
+        track_positions[track_id] = {'positions': {}, 'artists': '', 'title': ''}
+        
+        # Fetch track details (artist names and song title)
+        track_details = sp.track(track_id)
+        artists = ', '.join(artist['name'] for artist in track_details['artists'])
+        title = track_details['name']
+        
+        # Store artist names and song title
+        track_positions[track_id]['artists'] = artists
+        track_positions[track_id]['title'] = title
 
     for playlist_name, playlist_id in playlists_dict.items():
         results = sp.playlist_tracks(playlist_id)
-        position = 0  # Reset position for each playlist
+        position = 0
 
         while results:
             for item in results['items']:
                 if item['track'] and item['track']['id'] in track_ids:
                     track_id = item['track']['id']
-                    # Store the position in the corresponding dictionary
-                    if playlist_id not in track_positions[track_id]:
-                        track_positions[track_id][playlist_name] = []
-                    track_positions[track_id][playlist_name].append(position)
-                position += 1  # Increment position counter
+                    # Append position to the list for the corresponding track_id
+                    if playlist_name not in track_positions[track_id]['positions']:
+                        track_positions[track_id]['positions'][playlist_name] = []
+                    track_positions[track_id]['positions'][playlist_name].append(position)
+                position += 1
             results = sp.next(results) if results['next'] else None
 
     return track_positions
-
