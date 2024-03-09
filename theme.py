4171092d13cc1ebe1caeae6465d3e0a96ecccbb7
amcl11 <132857824+amcl11@users.noreply.github.com>
2024-03-09 16:33:40 +1100

    theme update

--- theme.py
+++ theme.py
@@ -1,28 +1,3 @@
-# import streamlit as st
-
-# def set_theme():
-  
-#     st.markdown(
-#         """
-#         <style>
-#         .stApp {
-#             background-color: #000000;
-#             color: #FAFAFA;
-#         }
-#         .stMarkdown h1, .stMarkdown h2, .stMarkdown h3, .stMarkdown h4, .stMarkdown h5, .stMarkdown h6 {
-#             color: #FAFAFA;
-#         }
-#         .stButton button {
-#             background-color: #055483;
-#             color: #FAFAFA;
-#         }
-#         </style>
-#         """,
-#         unsafe_allow_html=True,
-#     )
-
-
-
 import streamlit as st
 
 def set_theme():
@@ -41,37 +16,27 @@ def set_theme():
         }
 
         /* Sidebar background */
-        .stSidebar > div:first-child {
-            background-color: #055483;
-            color: #055483;
+        .stSidebar > div:first-child, .stSidebar .css-1aumxhk {
+            background-color: #055483 !important;
+            color: #FAFAFA !important;
         }
 
-        /* Sidebar headings */
-        .stSidebar .stMarkdown h1, .stSidebar .stMarkdown h2, .stSidebar .stMarkdown h3, .stSidebar .stMarkdown h4, 
-        .stSidebar .stMarkdown h5, .stSidebar .stMarkdown h6 {
-            color: #FAFAFA;
-        }
-
-        /* Sidebar widgets */
-        .stSidebar .stSelectbox .css-2b097c-container {
-            background-color: #055483;
-            color: #055483;
+        /* Sidebar selectbox, adjust class name according to the latest Streamlit version */
+        .stSelectbox > div, .css-2b097c-container {
+            background-color: #055483 !important;
+            color: #FAFAFA !important;
         }
 
         /* Adjust selectbox dropdown color */
-        .stSelectbox .css-1okebmr-indicatorSeparator, .stSelectbox .css-1hwfws3 {
-            background-color: #055483;
-        }
-
-        /* Adjust selectbox text and arrow color */
-        .stSelectbox .css-1uccc91-singleValue, .stSelectbox .css-1gtu0rj-indicatorContainer {
-            color: #055483;
+        .css-26l3qy-menu {
+            background-color: #055483 !important;
+            color: #FAFAFA !important;
         }
 
         /* Button styles in sidebar */
         .stSidebar .stButton button {
-            background-color: #055483;
-            color: #055483;
+            background-color: #055483 !important;
+            color: #FAFAFA !important;
         }
         </style>
         """,
