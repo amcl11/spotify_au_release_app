b1b86ac783a8bdf06e552ee2ffcb5e9ebffe08de
amcl11 <132857824+amcl11@users.noreply.github.com>
2024-03-25 11:06:24 +1100

    sidebar colour change

--- setup.sh
+++ setup.sh
@@ -16,6 +16,6 @@ port = $PORT\n\
 backgroundColor=\"#0E1117\"\n\
 primaryColor=\"#1db954\"\n\
 textColor=\"#FAFAFA\"\n\
-secondaryBackgroundColor=\"#224561\"\n\
+secondaryBackgroundColor=\"#162736\"\n\
 font=\"sans serif\"\n\
 " > ~/.streamlit/config.toml
