6649c4e9de92ad254add4b0a6d337a5a440bff67
amcl11 <132857824+amcl11@users.noreply.github.com>
2024-03-30 17:58:14 +1100

    move contact to about.py

--- pages/3_about.py
+++ pages/3_about.py
@@ -116,6 +116,22 @@ st.write("-----")
 
 # st.write("The schedule below outlines the current data pull frequency from Spotify's API.")
 
+st.markdown(
+    """
+    <style>
+        .my-text {
+            font-size: 14px;
+            font-family: monospace;
+        }
+    </style>
+    <div class="my-text">
+        Contact: 
+        <br>
+        <a href="mailto:new.music.playlist.tracker@gmail.com">new.music.playlist.tracker@gmail.com</a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 
 
